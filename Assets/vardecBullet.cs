6b79d8b12b657af781c2c674c84600b77055d5e3
Cece209 <143728721+Cece209@users.noreply.github.com>
2024-05-04 17:25:58 -0500

    Updated vardec
    gave vardec ability to shoot but still need some funcationalty

---
+++ Assets/vardecBullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vardecBullet : MonoBehaviour
+{
+    public float vardecBulletSpeed = 30f;
+    public Rigidbody2D vardecRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vardecRb.velocity = transform.right * vardecBulletSpeed;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+
+        Debug.Log(hitInfo.name);
+    }
+}
