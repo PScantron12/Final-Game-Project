da290737bdfce48eab7f23dfa078e0da0ffdfde0
Alex <apurce5@lsu.edu>
2024-05-10 01:02:26 -0500

    Boss Level Pause Menu Added

---
+++ Assets/Coded Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public float changeTime;
+    public string sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if(changeTime <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+        
+}
